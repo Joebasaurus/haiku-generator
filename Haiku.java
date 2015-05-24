9146f97eddf5734a7f0eb3531060d889ea9c17c8
Joebasaurus <jobingiannitti@gmail.com>
2015-05-23 23:47:15 -0400

    Update Haiku.java

--- Haiku.java
+++ Haiku.java
@@ -5,6 +5,7 @@ import java.awt.event.*;
 import java.io.*;
 import java.util.Random;
 import java.util.Set;
+
 import javax.swing.*;
 import javax.swing.text.*;
 
@@ -13,26 +14,31 @@ import javax.swing.text.*;
  * part of speech and syllabic order.
  * 
  * @author Jobin
- * @version 0.3.0
+ * @version 0.3.2
  */
-public class Haiku extends JFrame implements ActionListener {
+public class Haiku extends JFrame implements ActionListener 
+{	
+	private static final long serialVersionUID = 1L;
 	
-  // =========================== INTERNAL COMPONENTS =========================== \\
-     
-		//--- stores desired sentence structure
+     // =========================== INTERNAL COMPONENTS =========================== \\
+
+		//stores desired sentence structure
 		private static final SentenceGraph graph = new SentenceGraph();
 	
-		//--- stores information about loaded words
+		//stores information about loaded words
 		private Dictionary dictionary;
+	
 
-		//--- GUI components
+		// GUI components
 		private JButton generateButton;
 		private JTextPane output;
+
 	
 	
- // =========================== CONSTRUCTOR AND MAIN =========================== \\
+     // =========================== CONSTRUCTOR AND MAIN =========================== \\
 	
-	public Haiku() {
+	public Haiku() 
+	{		
 		setupDictionary();	
 		setupWindow();
 		System.out.println("   SETUP COMPLETE");
@@ -40,100 +46,125 @@ public class Haiku extends JFrame implements ActionListener {
 	
 	
 	public static void main(String[] args) {
-		//create an instance of 'Haiku' to invoke the setup functions
-		Haiku program = new Haiku();
+		new Haiku();
 	}
 	
 	
-	/**
-	 * Catch an ActionEvent -- used for identification of button clicks
-	 */
-	public void actionPerformed(ActionEvent e) {
-		if (e.getSource() == generateButton)
-			output.setText(generate());
-	}
-	
- // ============================ PRIMARY METHODS ================================ \\
+     // ============================ PRIMARY METHODS ================================ \\
      
 	/**
 	 * The backbone of the program.
 	 * @return a complete haiku.
 	 */
-	private String generate() {
+	public String generate() 
+	{
 		System.out.print("   Generating a haiku...");		
 		graph.reset();
 		
 		String[] outString = new String[3];
+		
 		do {
 			outString[0] = buildSentence(5, graph.getIndex());
-			if(graph.reachedEnd())
-				graph.reset();
-			outString[1] = buildSentence(7, graph.getIndex());
-			if(graph.reachedEnd())
-				graph.reset();
-			outString[2] = buildSentence(5, graph.getIndex());
-		} while (outString[0] == null || outString[1] == null || outString[2] == null);
 			
-		//-- capitalize first letter
+			if(graph.reachedEnd())
+				graph.reset();
+			
+			outString[1] = buildSentence(7, graph.getIndex());
+			
+			if(graph.reachedEnd())
+				graph.reset();
+			
+			outString[2] = buildSentence(5, graph.getIndex());
+		} 
+		while (containsNull(outString));
+			
+		//capitalize first letter
 		outString[0] = outString[0].substring(0, 1).toUpperCase() + outString[0].substring(1);
 		
 		System.out.println("done");
 		
-		return " " + outString[0] + "\n " + outString[1] + "\n " + outString[2];
+		//-- consolidate strings for output
+		String haiku = "";
+		for (int i = 0; i < outString.length; i++)
+			haiku += " " + outString[i] + "\n";
+		
+		return haiku;
+	}
+	 
+	
+	private boolean containsNull(String[] array)
+	{		
+		for (int i = 0; i < array.length; i++)
+			if (array[i] == null)
+				return true;
+		
+		return false;
 	}
 	
 	
 	/**
 	 * This method recursively traverses the supporting sentence structure graph.
-	 *
+	 * 
 	 * @param syllableCount the number of syllables remaining in the current line.
 	 * @param startIndex the index of the current graph node.
 	 * @return a string containing the current haiku line
 	 */
-	private String buildSentence(int syllablesLeft, int startIndex) {
+	private String buildSentence(int syllablesLeft, int startIndex) 
+	{
 		
-		//-- BASE CASE: the current line contains exactly (target) syllables
+		//BASE CASE: the current line contains exactly (target) syllables
 		if (syllablesLeft <= 0)
 			return "";
 		
-		//-- BASE CASE: end of sentence is reached
+		//BASE CASE: end of sentence is reached
 		if (startIndex >= graph.size() - 2 && syllablesLeft <= 0)
 			return "";
 		
 		
-		//-- Pick a word (in this call) to add. If the dictionary runs out, or if 0 syllables are specified,
+		//Pick a word (in this call) to add. If the dictionary runs out, or if 0 syllables are specified,
 		// this will return null.
 		PartOfSpeech nextPos = graph.getNode(startIndex);
 		String word = nextWord(nextPos, syllablesLeft);
 		
-		//-- if (word == null), no words can be found that meet the criteria, and you need a bigger dictionary.
+		// if (word == null), no words can be found that meet the criteria.
 		if(word != null) {
 			
-			//-- Iterate through the edges accessible from this position
+			// Iterate through the edges accessible from this position
 			int i = graph.nextEdge(startIndex);
 			
-			//-- this stops the sentence from ending on a preposition or article
-			if((graph.reachedEnd() || syllablesLeft - Dictionary.syllableCount(word) < 1) 
-			  (nextPos == PartOfSpeech.ARTICLE || nextPos == PartOfSpeech.PREPOSITION)) {
-				  System.out.println(" Error: cannot end on a preposition or article. (BACKTRACKING)");
-				  return null;
-			  }
+			//this stops the sentence from ending on a preposition or article
+			if(graph.reachedEnd() || syllablesLeft - Dictionary.sylCount(word) <1)
+					if(nextPos == PartOfSpeech.ARTICLE || nextPos == PartOfSpeech.PREPOSITION ) {
+						System.out.println(" Error: cannot end on a preposition or article. (BACKTRACKING)");
+						return null;
+					}
 			
 			while (graph.hasNextEdge(i) && i < graph.size() - 1) {
 				
-				//-- attempt travel to the next available edge
+				//attempt travel to the next available edge
 				System.out.println("attempting travel to edge: " + i + "    (pos: " + graph.getNode(i) + ")");
-				String temp = buildSentence(syllablesLeft - Dictionary.syllableCount(word), i);
+				String temp = buildSentence(syllablesLeft - Dictionary.sylCount(word), i);
 				
-				//-- if sentence can be completed by following this edge, commit the result.
-				//-- if (temp == null), method is backtracking (a dead end was reached in subsequent recursion).
-				if (temp != null)
+				// if sentence can be completed by following this edge, commit the result.
+				// if (temp == null), method is backtracking (a dead end was reached in subsequent recursion).
+				if (temp != null) {
+					
+					if (!(graph.reachedEnd() || syllablesLeft - Dictionary.sylCount(word) <1)) {
+						if(nextPos == PartOfSpeech.ADVERB) // this call is an adverb
+							if((i != 2 || i != 8) && (i != 6))	   // next call is not a prep or verb
+								word = word.trim() + ", ";
+						if(nextPos == PartOfSpeech.ADJECTIVE) // this call is an adjective
+							if(i == 4 || i == 10)			  // next call is an adjective
+								word = word.trim() + ", ";
+					}
+					
 					return word + temp;
+				}
 				
 				i = graph.nextEdge(i);
 			}
 		}
-		//-- if this point is reached, the method either has no more available edges or no words.
+		// if this point is reached, the method either has no more available edges or no words.
 		System.out.println("\n           DEAD END -- BACKTRACKING\n");
 		return null;
 	}
@@ -142,7 +173,6 @@ public class Haiku extends JFrame implements ActionListener {
 	
 	/**
 	 * Pick a random word from the dictionary that fits the given criteria.
-	 *
 	 * @param pos the desired part of speech
 	 * @param sMax the MAXIMUM number of syllables that the word can have
 	 */
@@ -151,16 +181,16 @@ public class Haiku extends JFrame implements ActionListener {
 		System.out.println(" Searching for a " + pos + " with <" + sMax + " syllables...");
 		
 		if (pos == PartOfSpeech.BLANK)
-			return "";  // <-- Advances sentence without using syllables or triggering backtracking
+			return "";  // Advances sentence without using syllables or triggering backtracking
 		if (sMax <= 0)
 			return null;
 		
-		//-- Create a set of all words that meet desired criteria
+		// Create a set of all words that meet desired criteria
 		Set<String> words = dictionary.wordSet(pos, 1, sMax);
 		if(words.size() == 0)
 			return null;
 		
-		//-- Choose one word from this set at random
+		// Choose one word from this set at random
 		int target = new Random().nextInt(words.size());
 		
 		int i = 0;
@@ -179,19 +209,20 @@ public class Haiku extends JFrame implements ActionListener {
 	/**
 	 * Initialize the supporting data structure for a Haiku generator.
 	 */
-	private void setupDictionary() {
+	private void setupDictionary() 
+	{
+		final String dictFileName = "dictionary.txt";
 		
-		//-- Deal with checked file IO exceptions
+		
 		try {
-			dictionary = new Dictionary();			
-		} catch (IOException exception) {
+			dictionary = new Dictionary(dictFileName);	
+		} 
+		catch (IOException exception) {
 			
-			//-- print exception info to console
 			exception.printStackTrace();
-			
-			//-- display error message
 			JOptionPane.showMessageDialog(this, exception.getMessage(), this.getTitle(), 
 					JOptionPane.ERROR_MESSAGE);
+			
 			System.exit(1);
 		}
 	}
@@ -200,29 +231,34 @@ public class Haiku extends JFrame implements ActionListener {
 	/**
 	 * Initialize the GUI components of a Haiku generator window.
 	 */
-	private void setupWindow() {
-		System.out.print("  Creating GUI window...");		
-		this.getContentPane().setLayout(new BorderLayout());
+	private void setupWindow() 
+	{
+		System.out.print("  Creating GUI window...");
+		JPanel background = new JPanel(new BorderLayout());
 		
-		//-- setup generation button
+		//setup generation button
 		generateButton = new JButton("Haiku");
 		generateButton.addActionListener(this);
-		this.getContentPane().add(generateButton, BorderLayout.SOUTH);
+		background.add(generateButton, BorderLayout.SOUTH);
 		
-		//-- setup output field 
+		//setup output field 
 		output = new JTextPane();
 		output.setEditable(false);
-		this.getContentPane().add(output, BorderLayout.CENTER);
-		output.setText("   Clicking down below \n"
-				+ "   will generate a haiku \n"
-				+ "   why don't you try it?");
+		//output.setLineWrap(true);
+		output.setText("   A click below this, \n"
+					+ "   and a haiku will appear. \n"
+					+ "   Why don't you try it?");
+		background.add(output, BorderLayout.CENTER);
 		
+		this.getContentPane().add(background);
+		
+		//-- set text to center-alignment
 		StyledDocument doc = output.getStyledDocument();
 		SimpleAttributeSet center = new SimpleAttributeSet();
 		StyleConstants.setAlignment(center, StyleConstants.ALIGN_CENTER);
 		doc.setParagraphAttributes(0, doc.getLength(), center, false);
 		
-		//-- window setup (general)
+		//window setup -- general
 		this.setTitle("Haiku Generator");
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		this.setLocationRelativeTo(null);
@@ -230,5 +266,14 @@ public class Haiku extends JFrame implements ActionListener {
 		this.setVisible(true);
 		
 		System.out.println("done");
+	}
+	
+	
+	/**
+	 * Catch an ActionEvent -- used for identification of button clicks
+	 */
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == generateButton)
+			output.setText(generate());
 	}
 }
